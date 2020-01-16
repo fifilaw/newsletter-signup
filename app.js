4c05c165e71b32f2cdba53e7047c02a915a90022
Fifi Law <59303902+fifilaw@users.noreply.github.com>
2020-01-16 12:39:00 -0800

    add list for subscription

--- app.js
+++ app.js
@@ -19,9 +19,40 @@ app.post("/",function(req, res){
   var lastName = req.body.lastName;
   var email = req.body.email;
 
+  var data ={
+    members:[
+      {email_address: email,
+      status: "subscribed"
+    }
+    ]
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  var options = {
+    url: "https://us4.api.mailchimp.com/3.0/lists/8bed3c8a42",
+    method:"POST",
+    headers: {
+      "Authorization": "fifi a023c44b1cfa942165344bc586bf5c00-us4"
+    },
+    body: jsonData
+  };
+
+  request(options, function(error, response, body){
+    if(error){
+      console.log(error);
+    }else{
+      console.log(response.statusCode);
+    }
+  });
+
   console.log(firstName, lastName, email);
 });
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000");
 });
+
+//id: 8bed3c8a42
+
+// a023c44b1cfa942165344bc586bf5c00-us4
