4b72412130352922693705f546be417624ae2595
Fifi Law <59303902+fifilaw@users.noreply.github.com>
2020-01-15 23:51:11 -0800

    create basic newsletter UI

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+
+  res.sendFile(__dirname+"/signup.html");
+
+});
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.post("/",function(req, res){
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var email = req.body.email;
+
+  console.log(firstName, lastName, email);
+});
+
+app.listen(3000, function(){
+  console.log("Server is running on port 3000");
+});
